66e3195a68e0e0f21e4faea112af29e35f282950
4750decoy <44045108+4750decoy@users.noreply.github.com>
2021-06-01 14:02:41 +0800

    Hover Selector
    When hovering the task div (pomodoro).  Making the inside div change

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-// JavaScript Document
+//JavaScript Document
+
+//  Ensure small window size could have fixed nav  //
 window.addEventListener('scroll',function(){
 	var nav = document.querySelector('#Nav');
 	nav.classList.toggle('sticky',window.scrollY > 0);
 })
 
-let time = '0';
+
+//  Get time in xx:xx:xx format  //
 
 function getTime(){
 	var t = new Date();
 	var H = String(t.getHours());
 	var M = String(t.getMinutes());
 	var S = String(t.getSeconds());
-	time = H+' : '+M+' : '+S;
+	var time = H+' : '+M+' : '+S;
 	document.getElementById("clock").innerHTML = time;
 }
 
+
+//  Refresh every 1 second  //
 window.setInterval('getTime()',100);
 
+
+//  hide other boxs  //
 function changebox(id){
 	var box = document.getElementById(id);
 	document.getElementById('pomodoro').style.display = 'none';
@@ -26,8 +33,11 @@ function changebox(id){
 	box.style.display = 'block';
 }
 
-function hide(id){
-	document.getElementById(id).style.display = 'none';
+
+// hide ohter div when switch   //
+function hide(id1,id2){
+	document.getElementById(id1).style.display = 'none';
+	document.getElementById(id2).style.display = 'block';
 }
 
 
