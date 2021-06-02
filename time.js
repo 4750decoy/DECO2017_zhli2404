141174d57f33c152cefc09da3a703df56dce6a5c
4750decoy <44045108+4750decoy@users.noreply.github.com>
2021-06-02 19:18:32 +0800

    Input style
    Make input form nicer

--- time.js
+++ time.js
@@ -17,7 +17,7 @@ function getTime(){
 		S = '0'+S;
 	};
 	var time = H+' : '+M+' : '+S;
-	document.getElementById("houre").innerHTML = H;
+	document.getElementById("hour").innerHTML = H;
 	document.getElementById("minute").innerHTML = M;
 	document.getElementById("second").innerHTML = S;
 }
@@ -25,3 +25,26 @@ function getTime(){
 
 //  Refresh every 1 second  //
 window.setInterval('getTime()',100);
+
+
+function setTime(){
+	var t = new Date();
+	var H = '00';
+	var M = '00';
+	var S = '00';
+	if (H.length<2){
+		H = '0'+H;
+	};
+	if (M.length<2){
+		M = '0'+M;
+	};
+	if (S.length<2){
+		S = '0'+S;
+	};
+	var time = H+' : '+M+' : '+S;	
+	document.getElementById("hour_s").innerHTML = H;
+	document.getElementById("minute_s").innerHTML = M;
+	document.getElementById("second_s").innerHTML = S;
+}
+
+window.setInterval('setTime()',100);
