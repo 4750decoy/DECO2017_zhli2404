18272c7d59583f23b2c6b6014fe4aa2933ddc019
4750decoy <44045108+4750decoy@users.noreply.github.com>
2021-06-04 22:17:28 +0800

    Delete Task from html level

--- kanban.js
+++ kanban.js
@@ -1,101 +1,131 @@
 // JavaScript Document
+localStorage.clear();
+var colum  = 1;
 
 
 var dragsave = 0;
 var dragenter = 0;
+
+
+var defpage = 'homepage';
+var tempTask = 0;
 //  Setting the title row to be able to create new element to make multiple columns  //
 function newCol(){
 	var newBut = document.createElement('button');
 	var add = document.getElementById('createB');
+	add.style.display = 'inline-block';
 	newBut.classList.add('kbHead');
-	newBut.innerHTML = 'hahah';
-	
+	newBut.innerHTML = 'Page'+String(colum);
 	// add the new tab(column) to the right before the add button //
 	document.getElementById('kbTitle').insertBefore(newBut,add);
+	colum += 1;
 }
+
+
+
+
+
+
+
 
 
 // Add new task rows  //
 function newRow(){
-
-	
 	// Before creating new rows, get infos needed from the task create div //
 	var name = document.getElementById('nameEntry').value;
 	var date = document.getElementById('dateEntry').value;
 	var est = document.getElementById('timeEntry').value;
 	var statu = document.getElementById('statusEntry').value;
 	var prio = document.getElementById('prioEntry').value;
-	
+	var page = defpage;
 	
 	
 	if (name.length>0 && date.length>0 && est.length>0 && statu.length>0 && prio.length>0){
 		//  call createData function to create localstorage (could be session storage to clearout, but prototype need to use one)  //
-		var set =new createData(name,date,est,statu,prio);
-
-		localStorage.setItem(name,set);
+		var set = {sname:name,sdate:date,sest:est,sstatu:statu,sprio:prio,spage:page};
+		//   LocalStorage can save paire of strings, when storing Object, we can store them using JSON and using parse to retrieve//
+		localStorage.setItem(name,JSON.stringify(set));  // This method is studied at https://www.jianshu.com/p/9d06304d35b7  //
 
 		//  new rows should be in the last when created //
-		var row = document.createElement('div');
-		row.innerHTML = '&nbsp &nbsp '+String(name)+'&nbsp &nbsp '+String(date)+'&nbsp &nbsp '+String(est)+'&nbsp &nbsp '+String(statu)+'&nbsp &nbsp '+String(prio);
-		document.getElementById('kbView').appendChild(row);
-		row.id = name;
-		row.classList.add('taskBar');
-		row.draggable = 'true';
-		
-		
-		//  Adding Drag Event  //
-		row.addEventListener('dragstart',function(event){
-			dragsave = row;
-		});
-		
-		
-		//  Record the event target and move it afterwards  //
-		row.addEventListener('dragenter',function(event){
-			dragenter = row;
-			event.preventDefault();
-			document.getElementById('kbView').insertBefore(dragsave,this.nextSibling);
-		});
-		
-		row.addEventListener('dragleave',function(event){
-			event.preventDefault();
-			document.getElementById('kbView').insertBefore(dragsave,dragenter);
-		});
-
-		
-		
-		
-
 	}else{
 		alert("Do not left the blank empty");
 	}
 	
 	
 	// Make color change according to priority //
-	if (prio == 'Low'){
-		row.style.background = 'lightgreen';
-	}else if (prio == 'Medium'){
-		row.style.background = 'orange';
-	}else if (prio == 'High'){
-		row.style.background = 'red';
+
+	getAll();
+}
+
+
+
+
+
+function getAll(){
+	var storeLen = localStorage.length;
+	for (var i = 0; i < storeLen;i++){
+		var getkey = localStorage.key(i);
+		var getvalue = localStorage.getItem(getkey);
+		var data = JSON.parse(getvalue);
+		if (data.spage == 'homepage'){
+			var row = document.createElement('div');
+			row.innerHTML = ' &nbsp &nbsp '+String(data.sprio)+'&nbsp &nbsp &nbsp'+String(data.sdate)+'&nbsp &nbsp &nbsp'+String(data.sname)+'&nbsp &nbsp &nbsp EST: '+String(data.sest)+'&nbsp &nbsp &nbsp'+String(data.sstatu);
+			document.getElementById('kbView').appendChild(row);
+			row.id = data.sname;
+			row.classList.add('taskBar');
+			row.draggable = 'true';
+
+
+			//  Adding Drag Event  //
+			row.addEventListener('dragstart',function(event){
+				dragsave = row;
+			});
+
+
+				//  Record the event target and move it afterwards  //
+			row.addEventListener('dragenter',function(event){
+				dragenter = row;
+				event.preventDefault();
+				document.getElementById('kbView').insertBefore(dragsave,this.nextSibling);
+			});
+
+			row.addEventListener('dragleave',function(event){
+				event.preventDefault();
+				document.getElementById('kbView').insertBefore(dragsave,dragenter);
+			});
+			if (data.sprio == 'Low'){
+				row.style.background = 'lightgreen';
+			}else if (data.sprio == 'Medium'){
+				row.style.background = 'orange';
+			}else if (data.sprio == 'High'){
+				row.style.background = 'red';
+			}
+		}
 	}
 }
 
 
 
-//  Using function to create the dataset instead of using object ,this way is much simpler //
-function createData(id1,id2,id3,id4,id5){
-	this.name = id1;
-	this.due = id2;
-	this.est = id3;
-	this.statu = id4;
-	this.priority = id5;
-}
+//  Delete the element when dragged to the pointed area //
+document.getElementById('delArea').addEventListener('dragenter',function(event){
+	event.preventDefault();
+	this.style.border = '2px solid red';
+});
 
 
-function newTask(id1,id2,id3,id4){
-	var task = createData(id1,id2,id3,id4);
-	localStorage.setItem(id1,task);
-}
+
+//    !!!!!!!!!!!This took me too much time, dragover should prevendefault to let the element could be dropped!!!!!!!!!!!!!! //
+document.getElementById('delArea').addEventListener('dragover',function(event){
+	event.preventDefault();
+});
+
+
+//  Delete //
+document.getElementById('delArea').addEventListener('drop',function(event){
+	event.preventDefault();
+	dragsave.remove();
+});
+
 
 /*function getAll(){
 	var storeLen = localStorage.length;
