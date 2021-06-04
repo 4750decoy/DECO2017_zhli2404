3f34b787d6386127d850294066aab4d225e8acda
4750decoy <44045108+4750decoy@users.noreply.github.com>
2021-06-04 17:34:25 +0800

    Task Create Overflow
    Unlimited Task Creation

--- kanban.js
+++ kanban.js
@@ -1,32 +1,59 @@
 // JavaScript Document
 
+
+//  Setting the title row to be able to create new element to make multiple columns  //
 function newCol(){
 	var newBut = document.createElement('button');
 	var add = document.getElementById('createB');
 	newBut.classList.add('kbHead');
 	newBut.innerHTML = 'hahah';
+	
+	// add the new tab(column) to the right before the add button //
 	document.getElementById('kbTitle').insertBefore(newBut,add);
 }
 
+
+// Add new task rows  //
 function newRow(){
-	var row = document.createElement('div');
-	var lastRow = document.getElementById('task1');
-	row.classList.add('taskBar');
-	row.innerHTML = '&nbsp &nbsp haha';
-	document.getElementById('kbView').appendChild(row);
+
 	
-	var name = document.getElementById(taskName).value;
-	var date = document.getElementById(taskName).value;
-	var est = document.getElementById(taskName).value;
-	var 
+	// Before creating new rows, get infos needed from the task create div //
+	var name = document.getElementById('nameEntry').value;
+	var date = document.getElementById('dateEntry').value;
+	var est = document.getElementById('timeEntry').value;
+	var statu = document.getElementById('statusEntry').value;
+	var prio = document.getElementById('prioEntry').value;
+	
+	
+	
+	if (name.length>0 && date.length>0 && est.length>0 && statu.length>0 && prio.length>0){
+		//  call createData function to create localstorage (could be session storage to clearout, but prototype need to use one)  //
+		var set =new createData(name,date,est,statu,prio);
+
+		localStorage.setItem(name,set);
+
+		document.getElementById('task1').innerHTML = 'your';
+		//  new rows should be in the last when created //
+		var row = document.createElement('div');
+		var lastRow = document.getElementById('task1');
+		row.classList.add('taskBar');
+		row.innerHTML = '&nbsp &nbsp '+String(name)+'&nbsp &nbsp '+String(date)+'&nbsp &nbsp '+String(est)+'&nbsp &nbsp '+String(statu)+'&nbsp &nbsp '+String(prio);
+		document.getElementById('kbView').appendChild(row);
+	}else{
+		alert("Do not left the blank empty");
+	}
+
 }
 
 
-function createData(id1,id2,id3,id4){
+
+//  Using function to create the dataset instead of using object ,this way is much simpler //
+function createData(id1,id2,id3,id4,id5){
 	this.name = id1;
 	this.due = id2;
 	this.est = id3;
 	this.statu = id4;
+	this.priority = id5;
 }
 
 
